d9cb8ea651188d957aff7e25e43e0190cfddc763
saivignesh2001s <saivignesh2001s@gmail.com>
2022-08-22 07:54:52 +0530

    committed

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.VisualBasic;
+using System.Text;
+
+namespace student
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = System.IO.File.ReadAllText(@"C:\Users\saivignesh\Desktop\Rainbowschools.txt");
+            System.Console.WriteLine("Contents of Rainbowschools.txt = \n{0}", text);
+            Console.ReadLine();
+        }
+    }
+}
